d17c1ed99d34b7767ddad47d2008b7457e2bb94b
Bdarrier <benoit.darrier@gmail.com>
2022-12-20 09:54:12 +0100

    retour correct de la requete

--- main.py
+++ main.py
@@ -3,17 +3,8 @@ from fastapi import FastAPI
 from spacy.language import Language
 from fonction import *
 import pickle
+import pandas as pd
 
-
-# model loading
-model = pickle.load(open("PickledModel/model.pkl", 'rb'))
-binarized = pickle.load(open("PickledModel/binarizerr.pkl", 'rb'))
-vectorized = pickle.load(open("PickledModel/vectorizer.pkl", 'rb'))
-# prepare pipeline nlp
-Language.factory("language_detector", func=get_lang_detector)
-nlp = spacy.load("en_core_web_sm")
-nlp.add_pipe("language_detector", last=True)
-#
 
 #opening API
 app = FastAPI()
@@ -27,12 +18,22 @@ def get_tag_suggestion(titre,question):
     ___
     :return: 
     """
+    # model loading
+    model = pickle.load(open("PickledModel/model.pkl", 'rb'))
+    binarized = pickle.load(open("PickledModel/binarizerr.pkl", 'rb'))
+    vectorized = pickle.load(open("PickledModel/vectorizer.pkl", 'rb'))
+    # prepare pipeline nlp
+    Language.factory("language_detector", func=get_lang_detector)
+    nlp = spacy.load("en_core_web_sm")
+    nlp.add_pipe("language_detector", last=True)
+    #
     canned_soup=prepare_the_soup(question)
     titre_et_question='. '.join([str(titre) , str(canned_soup) ])
     tmp_text, tmp_lemma, tmp_sent_text, tmp_sent_lemma, langdict = lemmatisation(nlp(titre_et_question))
-    tfidf_question=vectorized.transform(tmp_lemma)
-    suggested_tag_matrix=model.predict(tfidf_question)
-    suggested_tag_words=binarized.inverse_transform(suggested_tag_matrix)
+    tfidf_question=vectorized.transform([tmp_lemma])
+    df_tfidf_question = pd.DataFrame(tfidf_question.toarray(), columns=vectorized.get_feature_names_out())
+    suggested_tag_matrix = model.predict(df_tfidf_question)
+    suggested_tag_words = binarized.inverse_transform(suggested_tag_matrix)
 #
-    return {"tags" : suggested_tag_words}
+    return suggested_tag_words
 
